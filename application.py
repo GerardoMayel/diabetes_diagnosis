97ef2c98c206067b11d1fcc4962f171b4f6e91eb
Gerardo Mayel Fernández Alamilla <geramfernandez@gmail.com>
2023-04-15 19:00:27 -0600

    se agrega codigo para application.py sin bugs

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 with open('./model/mejor_modelo.pkl', 'rb') as f:
     modelo = pickle.load(f)
 
-scaler = load('./model/scaler.joblib')
+preprocesador = load('./model/preprocessor.joblib')
 
 
 @app.route('/')
@@ -43,32 +43,20 @@ def resultado():
     alopecia = int(request.form['alopecia'])
     obesidad = int(request.form['obesidad'])
 
-    # Realizar transformaciones a los datos
-    genero_hombre = 0
-    genero_mujer = 0
-    if genero == 'Hombre':
-        genero_hombre = 1
-    elif genero == 'Mujer':
-        genero_mujer = 1
-
     # Definir los nombres de las columnas
-    column_names = ['edad', 'genero_Hombre', 'genero_Mujer', 'poliuria', 'polidipsia', 'perdida_subita_de_peso', 'debilidad', 'polifagia',
+    column_names = ['edad', 'genero', 'poliuria', 'polidipsia', 'perdida_subita_de_peso', 'debilidad', 'polifagia',
                     'candidiasis_genital', 'vision_borrosa', 'picazon', 'irritabilidad', 'curacion_lenta', 'paresia_parcial', 'rigidez_muscular', 'alopecia', 'obesidad']
 
-    datos = np.array([[edad, genero_hombre, genero_mujer, poliuria, polidipsia, perdida_subita_de_peso, debilidad, polifagia,
+    datos = np.array([[edad, genero, poliuria, polidipsia, perdida_subita_de_peso, debilidad, polifagia,
                      candidiasis_genital, vision_borrosa, picazon, irritabilidad, curacion_lenta, paresia_parcial, rigidez_muscular, alopecia, obesidad]])
 
     data_pred = pd.DataFrame(datos, columns=column_names)
-    data_pred = data_pred[['edad', 'poliuria', 'polidipsia', 'perdida_subita_de_peso', 'debilidad',
-                           'polifagia', 'candidiasis_genital', 'vision_borrosa', 'picazon',
-                           'irritabilidad', 'curacion_lenta', 'paresia_parcial',
-                           'rigidez_muscular', 'alopecia', 'obesidad', 'genero_Hombre',
-                           'genero_Mujer']]
-    # Preprocesamiento de la edad
-    data_pred['edad'] = scaler.transform([[edad]])
+
+    # Preprocesar los datos
+    data_preprocesada = preprocesador.transform(data_pred)
 
     # Realizar la predicción
-    prediccion = modelo.predict(data_pred)[0]
+    prediccion = modelo.predict(data_preprocesada)
 
     # Obtener el resultado
     resultado = ''
